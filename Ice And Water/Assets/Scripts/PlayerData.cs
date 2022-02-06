20d90cdf62d247b3c03bd80328f8f21db6757ccb
PredaKing2007 <91562271+PredaKing2007@users.noreply.github.com>
2022-02-06 16:08:50 +0300

    dfg

--- Ice And Water/Assets/Scripts/PlayerData.cs
+++ Ice And Water/Assets/Scripts/PlayerData.cs
@@ -6,17 +6,42 @@ public class PlayerData : MonoBehaviour
 {
     public bool isIce;
     public bool isFreeze;
-    // Update is called once per frame
-    void Update()
+    public float Range;
+    public string playerTag = "Player";
+    List<GameObject> target = new List<GameObject>();
+    void Awake()
     {
-        
+        target.Clear();
+        InvokeRepeating("CheckForPlayers", 0f, 1f);
     }
-
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    void CheckForPlayers()
     {
-        if (hit.gameObject.CompareTag("Player"))
+        GameObject[] players = GameObject.FindGameObjectsWithTag(playerTag);
+        if (players.Length > 1)
         {
-            Debug.Log("Ice");
+            float shortestDis = Mathf.Infinity;
+            List<GameObject> nearestPlayer = new List<GameObject>();
+            foreach (GameObject enemy in players)
+            {
+                float distance = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distance < shortestDis)
+                {
+                    shortestDis = distance;
+                    nearestPlayer.Add(enemy);
+                }
+            }
+            if (nearestPlayer.Count > 1 && shortestDis <= Range)
+            {
+                target = nearestPlayer;
+                Debug.Log(target.Count);
+            }
+            else
+                target = null;
         }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Range);
     }
 }
