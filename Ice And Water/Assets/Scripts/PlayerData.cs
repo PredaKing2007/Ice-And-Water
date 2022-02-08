29785c414d544bdb03fa550162e291704586c464
PredaKing2007 <91562271+PredaKing2007@users.noreply.github.com>
2022-02-08 15:50:12 +0300

    fg

--- Ice And Water/Assets/Scripts/PlayerData.cs
+++ Ice And Water/Assets/Scripts/PlayerData.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-public class PlayerData : MonoBehaviour
+public class PlayerData : MonoBehaviour,IPunObservable
 {
     public bool isIce;
     public bool isFreeze;
     private CharacterController controller;
+    
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -25,6 +27,18 @@ public class PlayerData : MonoBehaviour
         if (data != null)
         {
             data.isFreeze = true;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(isFreeze);
+        }
+        else if (stream.IsReading)
+        {
+            isFreeze = (bool)stream.ReceiveNext();
         }
     }
 }
